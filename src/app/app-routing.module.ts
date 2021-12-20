90c14c6050ca9efc826496be0dcc2348c2ae808b
Lautaro Osinaga <49956395+osinagalj@users.noreply.github.com>
2021-12-20 09:33:55 -0300

    change base path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: FoldersComponent },
+  { path: '', component: LoginComponent },
   { path: 'folders', component: FoldersComponent },
   { path: 'folders/:id', component: TasksComponent },
   { path: 'create-folder', component: CreateFolderComponent },
